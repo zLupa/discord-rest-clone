dd6b5c2fc787872cc235aa7363c95b14f21ec2a8
lupa <lupim@riddlehost.com>
2021-12-12 17:41:13 -0300

    :construction: Remove websocket for now

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,12 @@ import { io } from "./shared/infra/websocket/io";
 
 const server = http.createServer(app);
 
-io.attach(server, {
+// I will work on this later
+/* io.attach(server, {
   cors: {
     origin: "*",
   },
 });
+*/
 
 server.listen(3333, () => console.log("> Server is ready"));
